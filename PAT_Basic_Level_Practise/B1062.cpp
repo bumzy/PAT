38fae1f78f5b784b49d9e41937524d6957374f97
bumzy <bumzycm@gmail.com>
2017-07-17 21:21:56 +0800

    modify B1062

--- PAT_Basic_Level_Practise/B1062.cpp
+++ PAT_Basic_Level_Practise/B1062.cpp
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <math.h>
+#include <algorithm>
 using namespace std;
 
 int main(){
     int n1, m1, n2, m2, k;
     scanf("%d/%d %d/%d %d", &n1, &m1, &n2, &m2, &k);
+    if (n1 * m2 > n2 * m1) {
+        swap(n1, n2);
+        swap(m1, m2);
+    }
     int min_i = ceil((double)n1 / m1 * k);
     int max_i = floor((double)n2 / m2 * k);
     bool first = true;
