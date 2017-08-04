ea828d2fc9066731de7749275be6e050c05a6ca5
bumzy <bumzycm@gmail.com>
2017-08-04 11:53:55 +0800

    add A1122

---
+++ PAT_Advanced_Level_Practise/A1122.cpp
@@ -0,0 +1,49 @@
+/*************************************************************************
+	> File Name: A1122.cpp
+	> Author: bumzy
+	> Mail: bumzycm@gmail.com
+	> Created Time: Fri 04 Aug 2017 11:02:16 AM CST
+ ************************************************************************/
+
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+const int MAXN = 200 + 10;
+bool matrix[MAXN][MAXN] = {false};
+
+int main() {
+    int n, m;
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < m; ++i) {
+        int v1, v2;
+        scanf("%d%d", &v1, &v2);
+        matrix[v1][v2] = true;
+        matrix[v2][v1] = true;
+    }
+    int k;
+    scanf("%d", &k);
+    for (int i = 0; i < k; ++i) {
+        bool flag = true;
+        int t;
+        scanf("%d", &t);
+        vector<int> cycle(t);
+        for (int j = 0; j < t; ++j) {
+            scanf("%d", &cycle[j]);
+        }
+        if (cycle[0] != cycle[t - 1] || t != n + 1) {
+            flag = false;
+        } else {
+            for (int j = 0; j < t - 1; ++j) {
+                int u = cycle[j];
+                int v = cycle[j + 1];
+                if (!matrix[u][v]) {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        printf("%s\n", flag ? "YES" : "NO");
+    }
+    return 0;
+}
